8bec620ecc8788c861c239975c3a1dd73cd05b22
Daniel Woznicki & Kai Huang <pair+dwoznicki.huangkc@devbootcamp.com>
2016-01-05 10:40:09 -0800

    Move app folder to root directory and ipdate config file

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,46 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+    render json: @students
+  end
+
+  def show
+    render json: @student
+  end
+
+  def create
+    @student = Student.new(post_params)
+    if @student.save
+      render json: @student, status: :created, location: @student
+    else
+      err
+    end
+  end
+
+  def update
+    if @student.update(post_params)
+      head :no_content
+    else
+      err
+    end
+  end
+
+  def destroy
+    @student.destroy
+    head :no_content
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def post_params
+    params.permit(:title, :author, :date, :body)
+  end
+
+  def err
+    render json: @post.errors, status: :unprocessable_entity
+  end
+end
