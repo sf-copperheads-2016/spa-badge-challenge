712a80378be552b7bf170b66d8eea9051e5071dd
Danielle Cameron & Joe Marion & Nicole Yee <pair+danyell85.joemarion.themcny@devbootcamp.com>
2016-01-05 11:40:16 -0800

    Ajax Request Works

--- spa-badge/config/application.rb
+++ spa-badge/config/application.rb
@@ -22,5 +22,12 @@ module SpaBadge
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.api_only = true
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :patch, :head]
+      end
+    end
   end
 end
