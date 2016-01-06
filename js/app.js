a2bb502a9e52398c65609092d0a074a3f0daa85e
Daniel Woznicki & Kai Huang <pair+dwoznicki.huangkc@devbootcamp.com>
2016-01-06 11:47:48 -0800

    Dynamically update votes for badges

--- js/app.js
+++ js/app.js
@@ -21,9 +21,9 @@ var indexTemplate = function() {
     var theCompiledHtml = theTemplate(context);
     $('.content-placeholder').sethtml(theCompiledHtml);
   }).then(function(){
-    var showTemplate = function(event){
+    var showTemplate = function(event, id){
       event.preventDefault();
-      var id = this.getAttribute("href")
+      var id = id || this.getAttribute("href")
       var theTemplateScript = $("#show").html();
       var theTemplate = Handlebars.compile(theTemplateScript);
       $().request({
@@ -48,12 +48,12 @@ var indexTemplate = function() {
           $().request({
             type: 'POST',
             url: 'http://localhost:3000/votes?value='+value+'&badge_id='+id
+          }).then(function(){
+            showTemplate(event, id)
           })
         }
         $('.upvote').on('click', createVote)
         $('.downvote').on('click', createVote)
-
-
       })
     }
     $('a').on("click", showTemplate)
