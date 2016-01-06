7db0baac585d73d73eb8de7d0c87e65f72e0a90f
Danielle Cameron & Joe Marion & Nicole Yee <pair+danyell85.joemarion.themcny@devbootcamp.com>
2016-01-06 10:56:12 -0800

    Hide and show content

--- client-side/js/app.js
+++ client-side/js/app.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   var source = document.getElementById("person-template").innerHTML;
   var template = Handlebars.compile(source);
-
   miniQuery("h1").ajax({
     type: "GET",
     url: "http://localhost:3000/students"
@@ -10,24 +9,33 @@ window.onload = function () {
     var context = {person};
     var output = template(context);
     document.getElementById("nameoutput").innerHTML = output;
-  }).catch(function(){
-    console.log()
-  });
-  console.log("BEFORE AJAX");
-
-  miniQuery("a").on('click', function(){
-    console.log("IN AJAx")
-    console.log(this)
-      miniQuery("a").ajax({
+  }).then(function(){
+      miniQuery('p').hide();
+      miniQuery('a').on('click', function() {
+        var studentId = this.href.split("#")
+       miniQuery("a").ajax({
         type: "GET",
-        url: "http://localhost:3000/students/"+ href
-      }).then(function(data){
-      console.log(data)
-    }).catch(function(){
-      console.log("ERRROS")
-    })
+        url: "http://localhost:3000/students/" + studentId[1]
+      }).then(function(response){
+        var a = JSON.parse(response)
+        var b = a.id;
+        var c = "#" + b.toString();
+        console.log(c)
+        console.log("HERHEHREHRHER")
+        var x = document.getElementById(b.toString());
+        if (x.style.display == "none"){
+          x.style.display = "block"
+        } else {
+          x.style.display = "none"
+        }
+      })
+     });
+      // miniQuery('a').trigger('click');
   });
-
-  miniQuery("a").trigger("click");
-
 }
+
+
+
+
+
+
