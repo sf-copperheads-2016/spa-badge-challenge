6a45b2371bdc155307cb9fd4c8dde4e4ed11b6bf
Julian Bouma <julian@devbootcamp.com>
2016-01-06 10:41:23 -0800

    display the teachers with handlebars

--- js/app.js
+++ js/app.js
@@ -3,10 +3,20 @@ $.ready(function() {
     type: 'GET',
     url: 'http://localhost:3000/teachers',
     success: function(response) {
-      console.log(response);
+      var teachers = JSON.parse(response);
+      displayTeachers(teachers);
     },
     fail: function(response) {
       console.log(response);
     }
   })
+
+  function displayTeachers(teachers) {
+    var teacherTmpl = $('#teachers-template').innerHTML;
+    var template = Handlebars.compile(teacherTmpl);
+    var context = { teacher: teachers };
+    var compiledHTML = template(context);
+
+    $('#teacher-list').innerHTML = compiledHTML;
+  }
 });
