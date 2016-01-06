81a68f495f6c95be12dab42687d5a1ea212c650f
Julian Bouma <julian@devbootcamp.com>
2016-01-06 09:56:19 -0800

    seed data for badges

--- badge-server/db/seeds.rb
+++ badge-server/db/seeds.rb
@@ -6,3 +6,7 @@ Teacher.create(name: 'Sarah')
 Teacher.create(name: 'Shambhavi')
 Teacher.create(name: 'Stu')
 Teacher.create(name: 'Walker')
+
+walker = Teacher.find_by(name: 'Walker')
+walker.badges.create(phrase: 'The coolest', votes: 0)
+walker.badges.create(phrase: 'Most likely to eat pizza', votes: 0)
