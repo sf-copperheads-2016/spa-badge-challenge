b622f5000f08493109d604c7b46c07021a9f830d
Julian Bouma <julian@devbootcamp.com>
2016-01-06 22:54:42 -0800

    cookies and routing

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 $.ready(function() {
+  var nameIdMap = {};
 
   getTeachers();
+
+  window.onhashchange = checkHash;
 
   // set up handlebars helpers
   Handlebars.registerHelper('toLowerCase', function(str) {
     return str.toLowerCase();
   });
+
+  function checkHash() {
+    var teacherName = location.hash.substring(1, location.hash.length);
+    if (nameIdMap.hasOwnProperty(teacherName)) {
+      getTeacher(nameIdMap[teacherName])
+    }
+  }
 
   function getTeachers() {
     $.ajax({
@@ -13,8 +23,15 @@ $.ready(function() {
       url: 'http://localhost:3000/teachers',
       success: function(response) {
         var teachers = JSON.parse(response);
-        displayTeachers(teachers);
-        setTeacherListeners();
+        for (var i = 0; i < teachers.length; i++) {
+          nameIdMap[teachers[i].name.toLowerCase()] = teachers[i].id;
+        }
+        if (location.hash.length === 0) {
+          displayTeachers(teachers);
+          setTeacherListeners();
+        } else {
+          checkHash();
+        }
       },
       fail: ajaxFailed
     });
@@ -24,16 +41,29 @@ $.ready(function() {
     $('.vote').on('submit', function(event) {
       event.preventDefault();
       var formData = serializeFormData(this);
-      $.ajax({
-        type: 'POST',
-        url: 'http://localhost:3000/badges/vote',
-        data: formData,
-        success: function(response) {
-          getTeacher(teacher_id);
-        },
-        fail: ajaxFailed
-      });
+      var badgeId = formData.split('&')[0].split('=')[1];
+      if (alreadyVoted(badgeId)) {
+        alert('you already voted!!');
+      } else {
+        writeCookie(badgeId, "true");
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost:3000/badges/vote',
+          data: formData,
+          success: function(response) {
+            getTeacher(teacher_id);
+          },
+          fail: ajaxFailed
+        });
+      }
     });
+  }
+
+  function alreadyVoted(badge_id) {
+    if (getCookie(badge_id)) {
+      return true;
+    }
+    return false;
   }
 
   function serializeFormData(form) {
@@ -87,6 +117,7 @@ $.ready(function() {
   function setHomeListener() {
     $('#home-button').on('click', function(event) {
       event.preventDefault();
+      location.hash = '';
       getTeachers();
     });
   }
@@ -124,5 +155,27 @@ $.ready(function() {
 
   function ajaxFailed(response) {
     console.error(response);
+  }
+
+  // http://stackoverflow.com/questions/13154552/javascript-set-cookie-with-expire-time
+  function writeCookie(key, value, days) {
+    var date = new Date();
+
+    // Default at 365 days.
+    days = days || 365;
+
+    // Get unix milliseconds at current time plus number of days
+    date.setTime(+ date + (days * 86400000)); //24 * 60 * 60 * 1000
+
+    window.document.cookie = key + "=" + value + "; expires=" + date.toGMTString() + "; path=/";
+
+    return value;
+  }
+
+  // http://stackoverflow.com/questions/10730362/get-cookie-by-name
+  function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
   }
 });
